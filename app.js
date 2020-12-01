bec95b59ff84c8ee2e907cbaa744ec83d476ce3c
Scrirock <73163680+Scrirock@users.noreply.github.com>
2020-12-01 14:54:39 +0100

    fghd

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let paragraph = document.getElementsByClassName("paragraphe");
 
-for (let i = 0; i < 5; i++){
+for (let i = 0; i < paragraph.length; i++){
    if ((i+1)%2 === 0){
        continue;
    }
